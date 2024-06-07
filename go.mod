fdbfb12eb3822847c4c49ab8f8ed18521580d905
Pavel Snajdr <snajpa@snajpa.net>
2024-06-07 22:05:42 +0200

    v1

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module goresheat
 
-go 1.22.3
+go 1.21
 
 require (
 	github.com/gorilla/websocket v1.5.1 // indirect
--- goresheat.nix
+++ goresheat.nix
@@ -1,6 +1,6 @@
 { lib, buildGoModule }:                                                         
 buildGoModule {                                                                
   name = "goresheat";
-  vendorHash = "sha256-e4w0YHxa/ImM3DseWCugbuytn5TN666IO69Dl7B0vpc=";
+  vendorHash = "sha256-iVGS9bvZ01AKuaFt1XLOKp6gW1NnPYTk0LoZzjsNmTg=";
   src = ./.;
 }
